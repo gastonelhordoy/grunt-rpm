afe34a0ce6538c0c3657410e6ef85a3eae44aeea
Gaston Elhordoy <dbqf36@motorola.com>
2013-09-19 13:45:23 -0300

    Included grunt-release plugin for releasing the first version.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
 
     // Before generating any new files, remove any previously-created files.
     clean: {
-      tests: ['tmp'],
+      tests: ['tmp', 'target'],
     },
 
     // Configuration to be run (and then tested).
@@ -52,6 +52,10 @@ module.exports = function(grunt) {
     nodeunit: {
       tests: ['test/*_test.js'],
     },
+	
+	release: {
+	  
+	}
 
   });
 
@@ -62,12 +66,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-release');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'rpm', 'nodeunit']);
+  grunt.registerTask('release', ['jshint', 'release']);
 
+  grunt.registerTask('test', ['clean', 'nodeunit']);
+  
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', ['jshint'/*, 'test'*/]);
 
 };
